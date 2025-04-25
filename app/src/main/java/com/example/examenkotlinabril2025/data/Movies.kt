1609f1f1dbcd90ebe4e6f3a250a8f082b069c221
IOSEdwin <eramirezn23@icloud.com>
2025-04-25 13:26:35 +0200

    termiando el examen

---
+++ app/src/main/java/com/example/examenkotlinabril2025/data/Movies.kt
@@ -0,0 +1,21 @@
+
+package com.example.proytectmine.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie (
+    @SerializedName ("imdbID") val id: String,
+    @SerializedName("Title") val title:String,
+    @SerializedName("Year") val year: String,
+    @SerializedName("Poster") val imageURL : String,
+    @SerializedName("Plot") val plot: String,
+    @SerializedName("Runtime") val runtime: String,
+    @SerializedName("Director") val director: String,
+    @SerializedName("Genre") val genre: String,
+    @SerializedName("Country") val country: String)
+
+
+data class MoviesResponse (
+    @SerializedName("Search") val movies: List<Movie>,
+    @SerializedName("Response") val response: String,
+    @SerializedName("Error") val error: String?)
